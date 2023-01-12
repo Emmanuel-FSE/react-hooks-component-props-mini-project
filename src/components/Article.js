4f11611893243bab9eb8692f80b04f861b31bc94
Emmanuel-FSE <emmanuelmutisya254@gmail.com>
2023-01-12 23:48:50 +0300

    readtime change

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,8 +1,24 @@
-function Article ({post: {date = "January 1, 1970", preview, title}}){
+function Article ({post: {date = "January 1, 1970", minutes, preview, title}}){
+    function readtime(){
+        if(minutes <=5){
+            return "☕️ 3 min read";
+        }
+        else if(minutes <=10 && minutes>5){
+            return "☕️☕️ 10 min read";
+        }
+        else if(minutes <=15 && minutes>10){
+            return "☕️☕️☕️ 15 min read"
+        }
+        else if(minutes <=20 && minutes>15){
+            return "☕️☕️☕️☕️ 20 min read"
+        }
+
+    }
     return(
        <article>
         <h3>{title}</h3>
         <small>{date}</small>
+        <small>{readtime()}</small>
         <p>{preview}</p>
        </article> 
     )
